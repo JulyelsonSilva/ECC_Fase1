da7b299dbf257c60be5423fa01e9415f804b683c
JulyelsonSilva <julyelson@gmail.com>
2026-04-15 09:12:02 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,26 @@
+import mysql.connector
+from mysql.connector import errors as mysql_errors
 
+from config import DB_CONFIG
+
+
+def db_conn():
+    return mysql.connector.connect(**DB_CONFIG)
+
+
+def safe_fetch_one(cur, sql, params):
+    """SELECT ... LIMIT 1 com tratamento; retorna dict ou None."""
+    try:
+        cur.execute(sql, params)
+        return cur.fetchone()
+    except (
+        mysql_errors.ProgrammingError,
+        mysql_errors.DatabaseError,
+        mysql_errors.InterfaceError,
+        mysql_errors.OperationalError,
+    ):
+        return None
+
+
+def _get_db():
+    return db_conn()
