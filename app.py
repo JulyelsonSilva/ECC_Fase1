67c4e74d71c2c958d169bd922a8f9613eb374b85
JulyelsonSilva <julyelson@gmail.com>
2026-04-25 05:43:00 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ from config import (
     DB_CONFIG,
     TEAM_MAP,
     TEAM_LIMITS,
-    TEAM_CHOICES,
     PALESTRAS_TITULOS,
     PALESTRAS_SOLO,
 )
 
-from db import safe_fetch_one, _get_db
+from db import safe_fetch_one
 from auth import _admin_ok
 from utils import _norm, _sim, _q, _team_label
 from services.shared_service import encontrista_name_by_id
@@ -19,7 +18,6 @@ from routes.encontristas import register_encontristas_routes
 from routes.encontreiros import register_encontreiros_routes
 from routes.circulos import register_circulos_routes
 from routes.montagem import register_montagem_routes
-from routes.implantacao import register_implantacao_routes
 from routes.palestras import register_palestras_routes
 from routes.core import register_core_routes
 from routes.admin import register_admin_routes
@@ -33,7 +31,6 @@ register_encontristas_routes(app, encontrista_name_by_id)
 register_encontreiros_routes(app, PALESTRAS_TITULOS, PALESTRAS_SOLO, DB_CONFIG, safe_fetch_one)
 register_circulos_routes(app, encontrista_name_by_id)
 register_montagem_routes(app, TEAM_MAP, TEAM_LIMITS, _team_label)
-register_implantacao_routes(app, TEAM_MAP, TEAM_LIMITS, TEAM_CHOICES, _team_label)
 register_palestras_routes(app, PALESTRAS_TITULOS, PALESTRAS_SOLO)
 register_core_routes(app, TEAM_MAP, TEAM_LIMITS, _q)
 register_admin_routes(app, _admin_ok)
