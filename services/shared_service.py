fd5fe7d2a1ac50716f2769a437c1943b10e759ef
JulyelsonSilva <julyelson@gmail.com>
2026-04-25 20:22:57 -0300

    Atualizar o shared_service.py

--- services/shared_service.py
+++ services/shared_service.py
@@ -1,10 +1,27 @@
-def encontrista_name_by_id(conn, _id):
+def encontrista_name_by_id(conn, _id, paroquia_id=None):
     cur = conn.cursor(dictionary=True)
     try:
-        cur.execute("SELECT nome_usual_ele, nome_usual_ela FROM encontristas WHERE id=%s", (_id,))
+        if paroquia_id:
+            cur.execute("""
+                SELECT nome_usual_ele, nome_usual_ela
+                FROM encontristas
+                WHERE id = %s
+                  AND paroquia_id = %s
+            """, (_id, paroquia_id))
+        else:
+            cur.execute("""
+                SELECT nome_usual_ele, nome_usual_ela
+                FROM encontristas
+                WHERE id = %s
+            """, (_id,))
+
         r = cur.fetchone()
         if not r:
             return None, None
-        return (r.get("nome_usual_ele") or ""), (r.get("nome_usual_ela") or "")
+
+        return (
+            r.get("nome_usual_ele") or "",
+            r.get("nome_usual_ela") or ""
+        )
     finally:
         cur.close()
